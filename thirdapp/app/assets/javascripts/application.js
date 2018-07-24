5688578bc7516d8481ab4e085a908d70efe6e36a
sagarsaindre <bhargavi.ambekar@wgblindia.com>
2018-07-24 22:28:05 +0530

    8th app added

--- thirdapp/app/assets/javascripts/application.js
+++ thirdapp/app/assets/javascripts/application.js
@@ -9,7 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+
+//= require bootstrap
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- thirdapp/app/controllers/users_controller.rb
+++ thirdapp/app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
      @user = User.new(user_params)   # Not the final implementation!
     if @user.save
       # Handle a successful save.
+       log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
